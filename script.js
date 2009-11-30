a09c0d4f7bcae413b5a2f200df9ec72379e87d3c
legendar <legendar@gmail.com>
2009-11-30 20:43:59 +0200

    use new API

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 
 new function() {
     var o = this;
+
+    var options = {
+        maxWidth: 300,
+        color: 'fafafa',
+        background: '363636',
+        opacity: 0.8,
+        border: '000000',
+        borderWidth: 3,
+        fontSize: 12
+    }
     
     this.dialog = document.createElement('div');
     this.dialog.style.display = 'none';
-    this.dialog.className = 'auto-translate-dialog';
+    this.dialog.id = 'auto-translate-dialog';
+    this.dialog.setAttribute('style', 
+        'max-width: ' + options.maxWidth + 'px;' +
+        'color: #' + options.color + ';' +
+        'opacity: ' + options.opacity + ';' +
+        'border-color: #' + options.border + ';' +
+        'border-width: ' + options.borderWidth + 'px;' +
+        'background: #' + options.background + ';' +
+        'font-size: #' + options.fontSize + 'px;' +
+        'position: fixed; top: 0; left: 0;'
+    );
     document.body.appendChild(this.dialog);
     
     this.port = chrome.extension.connect();
     this.port.onMessage.addListener(function(m) {
         switch(m.message) {
             case 'result':
-                o.dialog.innerHTML = m.result.translation;
+                o.dialog.innerHTML = m.result;
                 o.dialog.style.display = '';
                 break;
         }
@@ -34,8 +54,19 @@ new function() {
 
     }, false);
     
-    document.body.addEventListener('mousedown', function() {
-        o.dialog.style.display = 'none';
+    document.body.addEventListener('mousedown', function(e) {
+        var r = true;
+        e = e.srcElement;
+        while(e.parentNode) {
+            if(e.id == 'auto-translate-dialog') {
+                r = false;
+                break;
+            }
+            e = e.parentNode;
+        }
+        if(r) {
+            o.dialog.style.display = 'none';
+        }
     }, false);
 }
 
